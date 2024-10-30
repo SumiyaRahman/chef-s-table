7ef08e9a5f46bff4a7309860bab8a9594fa8b7ec
SumiyaRahman <sumiarahman01739@gmail.com>
2024-10-29 23:27:15 -0600

    navbar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -23,12 +23,19 @@ const Header = () => {
       <div className="flex flex-row justify-between items-center gap-4">
         {/* search */}
         <div className="flex justify-between items-center relative">
-            <input className="py-4 px-10 bg-gray-100 rounded-full outline-none" type="text" placeholder="Search" />
+          <input
+            className="py-4 px-12 bg-gray-100 rounded-full outline-none placeholder:text-sm"
+            type="text"
+            placeholder="Search"
+          />
+          <div className="absolute inset-y-0 left-3 pl-4 flex items-center">
+            <i className="fa-solid fa-magnifying-glass text-sm text-gray-500"></i>
+          </div>
         </div>
         {/* search end */}
 
         <div className="bg-green-400 flex items-center justify-center rounded-full w-12 h-12">
-            <i className="fa-regular fa-user-circle text-3xl"></i>
+            <i className="fa-regular fa-user-circle text-gray-700 text-3xl"></i>
         </div>
       </div>
     </section>
